c8d1b405a74359767d12822b2583e76aa146b817
Chanda Mounica <pvryar.4@gmail.com>
2025-06-15 22:00:29 +0530

    Add project files.

---
+++ project1/Basic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+
+internal class Basic
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("This is my first c# code");
+        Console.WriteLine("--------------------------------------------------------");
+        getStudentDetails();
+        getPlayerDetails();
+    }
+
+    static void getStudentDetails()
+    {
+        Console.WriteLine("Please enter Student Name:");
+        String Name = Console.ReadLine();
+        Console.WriteLine("Student Name is:" + Name);
+
+        Console.WriteLine("Please enter Group of Student:");
+        String Group = Console.ReadLine();
+        Console.WriteLine($"Student Group is: {Group}");
+
+        Console.WriteLine("--------------------------------------------------------");
+    }
+
+    static void getPlayerDetails()
+    {
+        Console.WriteLine("Enter first Name:");
+        String FirstName = Console.ReadLine();
+        Console.WriteLine("Enter Last Name");
+        String LastName = Console.ReadLine();
+        Console.WriteLine("Player Name is:" + FirstName + " " + LastName);
+
+    }
+
+
+}
