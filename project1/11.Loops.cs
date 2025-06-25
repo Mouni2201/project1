d25425c5346e7e24ed950499f3036a5084176682
Chanda Mounica <pvryar.4@gmail.com>
2025-06-26 05:01:58 +0530

    loops

--- project1/11.Loops.cs
+++ project1/11.Loops.cs
@@ -9,7 +9,52 @@ class loops
 
         //}
 
+  
+  
+            int i = 0;
+            int sum = 0;
+            Console.WriteLine($"value before addition: {i}");
+            while (i < 5)
+            {
+                //console.writeline(i);
+                i = i + 1;
+                //sum = sum + i;
+                Console.WriteLine($"value after addition: {i}");
+            }
 
-    }
-}
+            int[] numbers = { 12, 45, 67 };
+            int sum1 = 0;
+            int count = 0;
+            while (count < numbers.Length)
 
+            {
+            Console.WriteLine($"Counting stage {count}");
+            Console.WriteLine($"Numbers list {numbers[count]}");
+            Console.WriteLine($"Before Counting, total amount {sum}");
+
+            sum = sum + numbers[count];
+                count++;
+
+            }
+
+            Console.WriteLine($"After Counting, total amount {sum}");
+
+
+            while (count < 10)    //we won't get output as the length is only 3
+
+            {
+                Console.WriteLine($"Counting stage {count}");
+                Console.WriteLine($"Numbers list {numbers[count]}");
+                Console.WriteLine($"Before Counting, total amount {sum}");
+
+                sum = sum + numbers[count];
+                count++;
+
+            }
+
+            Console.WriteLine($"After Counting, total amount {sum}");
+
+
+
+        }
+    } 
